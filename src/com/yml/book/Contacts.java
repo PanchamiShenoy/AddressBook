2085d5a76ce7afb4da72f15b4f337d126622fd4b
PanchamiShenoy <panchamishenoy16@gmail.com>
2021-09-02 20:20:41 +0530

    create contacts

---
+++ src/com/yml/book/Contacts.java
@@ -0,0 +1,28 @@
+package com.yml.book;
+
+public class Contacts {
+	private String firstName,lastName,address,city,state,zip,phoneNumber,email;
+	public Contacts(String firstName,String lastName,String address,String city,String state,String zip,String phoneNumber,String email)
+	{
+		this.firstName=firstName;
+		this.lastName=lastName;
+		this.address=address;
+		this.city=city;
+		this.state=state;
+		this.zip=zip;
+		this.phoneNumber=phoneNumber;
+		this.email=email;
+	}
+	
+	public void display()
+	{
+		System.out.println("First Name:"+firstName);
+		System.out.println("Last Name:"+lastName);
+		System.out.println("Address:"+address);
+		System.out.println("City:"+city);
+		System.out.println("State:"+state);
+		System.out.println("Zip:"+zip);
+		System.out.println("Phone number"+phoneNumber);
+		System.out.println("E-mail:"+email);
+	}
+}
