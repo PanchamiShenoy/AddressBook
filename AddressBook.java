26080f0b13ba94ad4b9dc92985ae450f9744508e
PanchamiShenoy <61827955+PanchamiShenoy@users.noreply.github.com>
2021-09-23 23:45:38 +0530

    Add files via upload

---
+++ AddressBook.java
@@ -0,0 +1,226 @@
+package com.yml.book;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public class AddressBook {
+	String addressBookName;
+	static Scanner sc = new Scanner(System.in);
+	static Map<String, Contact> addressBook;
+
+	AddressBook(String addressBookName) {
+		this.addressBookName = addressBookName;
+		this.addressBook = new HashMap<>();
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(addressBookName);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AddressBook other = (AddressBook) obj;
+		return Objects.equals(addressBookName, other.addressBookName);
+	}
+
+	public String getAddressBookName() {
+		return addressBookName;
+	}
+
+	public void setAddressBookName(String addressBookName) {
+		this.addressBookName = addressBookName;
+	}
+
+	/*
+	 * method to add contact
+	 */
+	public static void addContact() {
+		Contact contact;
+
+		System.out.println("Enter the person details");
+		System.out.println("enter the first name");
+		String fname = sc.nextLine();
+		System.out.println("enter the last name");
+		String lname = sc.nextLine();
+		System.out.println("enter the addres");
+		String address = sc.nextLine();
+		System.out.println("enter the city");
+		String city = sc.nextLine();
+
+		System.out.println("enter the state");
+		String state = sc.nextLine();
+		System.out.println("enter the zip code");
+		String zip = sc.nextLine();
+		System.out.println("enter the phone Number");
+		String phone = sc.nextLine();
+		System.out.println("enter the email Id ");
+		String email = sc.nextLine();
+		contact = new Contact(fname, lname, address, city, state, zip, phone, email);
+		String name = fname + " " + lname;
+		Set<String> keyset = addressBook.keySet();
+		Supplier<Stream<String>> streamSupplier = () -> keyset.stream();
+		Optional<String> result1 = streamSupplier.get().findAny();
+		if (result1.isEmpty()) {
+			System.out.println("Adding details");
+			addressBook.put(fname + " " + lname, contact);
+		} else {
+			if (streamSupplier.get().anyMatch(x -> x.equals(name))) {
+				System.out.println("There is already a person with this name ");
+			} else {
+				System.out.println("Adding details");
+				addressBook.put(fname + " " + lname, contact);
+			}
+		}
+
+	}
+	/*
+	 * method to search person by place name
+	 */
+	public void search(String place) {
+		Set<Map.Entry<String, Contact>> entries = addressBook.entrySet();
+		Stream<Map.Entry<String, Contact>> entriesStream = entries.stream();
+
+		Set<String> keySet = addressBook.keySet();
+		Collection<Contact> values = addressBook.values();
+
+		Stream<Contact> valuesStream = values.stream();
+		Stream<String> keysStream = keySet.stream();
+
+		valuesStream.anyMatch((x) -> {
+			if (x.city.equals(place) || x.state.equals(place)) {
+				System.out.println(x.firstName);
+				return true;
+			} else {
+				return false;
+			}
+		});
+
+	}
+	/*
+	 * method to edit contact
+	 */
+	public static void editPerson() {
+
+		System.out.println("enter the first name");
+		String fname = sc.nextLine();
+		System.out.println("enter the last name");
+		String lname = sc.nextLine();
+
+		String name = fname + " " + lname;
+
+		Contact c = addressBook.get(name);
+		if (c == null) {
+			System.out.println("Person of that name not exits in this book");
+		} else {
+			int choice;
+
+			Scanner r = new Scanner(System.in);
+
+			while (true) {
+				System.out.println("What do you wanna edit");
+				System.out.println(
+						"1 First Name\n2 Last Name\n3 Address\n4 City\n5 State\n6Zip\n7 Phone number\n8Email\n9Go back");
+				choice = r.nextInt();
+				switch (choice) {
+				case 1:
+					System.out.println("enter the first name");
+					String firstName = sc.nextLine();
+					// person.firstName = firstName;
+					c.firstName = firstName;
+					break;
+				case 2:
+					System.out.println("enter the last name");
+					String lastName = sc.nextLine();
+					c.lastName = lastName;
+					break;
+				case 3:
+					System.out.println("enter the address");
+					String address = sc.nextLine();
+					c.address = address;
+					break;
+				case 4:
+					System.out.println("enter the city");
+					String city = sc.nextLine();
+					c.city = city;
+					break;
+				case 5:
+					System.out.println("enter state");
+					String state = sc.nextLine();
+					c.state = state;
+					break;
+				case 6:
+					System.out.println("enter the zip code");
+					String zip = sc.nextLine();
+					c.zip = zip;
+					break;
+				case 7:
+					System.out.println("enter phone number");
+					String phone = sc.nextLine();
+					c.phoneNumber = phone;
+					break;
+				case 8:
+					System.out.println("enter email");
+					String email = sc.nextLine();
+					c.email = email;
+					break;
+				case 9:
+					return;
+				}
+			}
+
+		}
+
+	}
+
+	/*
+	 * method to delete contact
+	 */
+	public static void deleteperson() {
+
+		System.out.println("enter the first name");
+		String fname = sc.nextLine();
+		System.out.println("enter the last name");
+		String lname = sc.nextLine();
+
+		String name = fname + " " + lname;
+
+		Contact c = addressBook.get(name);
+		if (c == null) {
+			System.out.println("Person does not exist");
+
+		} else {
+			addressBook.remove(name);
+		}
+
+	}
+
+	/*
+	 * method to print conatct
+	 */
+	public void print() {
+
+		for (Contact c : addressBook.values()) {
+			c.display();
+		}
+
+	}
+
+}
+
+
+
+
---
+++ AddressBookMain.java
@@ -0,0 +1,156 @@
+package com.yml.book;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class AddressBookMain {
+
+	static Map<String, AddressBook> addressBook = new HashMap<>();
+	static Scanner r = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to AddressBook program");
+
+		final int EXIT = 7;
+
+		int choice = 0;
+
+		while (choice != EXIT) {
+
+			System.out.println(
+					"1 : Add AddressBook\n2 : Add Contact\n3 : Edit Contact\n4 : Delete Contact\n5 : Display Contact\n6.Search\n7."+ EXIT + " : to exit");
+			Scanner r = new Scanner(System.in);
+			Scanner sc = new Scanner(System.in);
+			choice = r.nextInt();
+
+			switch (choice) {
+			case 1:
+				addAddressBook();
+				break;
+
+			case 2:
+				addContact();
+				break;
+			case 3:
+				editContact();
+
+				break;
+			case 4:
+				deleteContact();
+				break;
+			case 5:
+				displayContact();
+				break;
+			case 6:
+				search();
+				break;
+			case EXIT:
+				System.exit(0);
+
+			}
+
+		}
+
+	}
+	private static void search() {
+		System.out.println("Enter the city or state name");
+		String place = r.nextLine();
+		for (Map.Entry<String, AddressBook> entry : addressBook.entrySet()) {
+			AddressBook obj = entry.getValue();
+			obj.search(place);
+		}
+
+	}
+
+	/**
+	 * method to delete contact
+	 */
+	private static void deleteContact() {
+		System.out.println("Enter the name of address book from which you wanna delete");
+
+		String bookName = r.nextLine();
+		AddressBook adBook = addressBook.get(bookName);
+		if (adBook != null) {
+			addressBook.get(bookName).deleteperson();
+			;
+		} else {
+			System.out.println("Book name not found");
+		}
+
+	}
+
+	/*
+	 * method to edit contact
+	 */
+	private static void editContact() {
+		System.out.println("Enter the name of address book to which you wanna edit");
+
+		// Scanner r =new Scanner(System.in);
+		String bookName = r.nextLine();
+		AddressBook adBook = addressBook.get(bookName);
+		if (adBook != null) {
+			addressBook.get(bookName).editPerson();
+		} else {
+			System.out.println("Book name not found");
+		}
+
+	}
+
+	/*
+	 * method to add addressbook
+	 */
+	private static void addAddressBook() {
+		System.out.println("Enter the name of new address book");
+
+		String bookName = r.nextLine();
+
+		AddressBook book = addressBook.get(bookName);
+		if (book != null) {
+			System.out.println("Already has a address book of that name");
+		} else {
+
+			AddressBook adBook = new AddressBook(bookName);
+			addressBook.put(bookName, adBook);
+		}
+
+	}
+
+	/*
+	 * method to display contact
+	 */
+	private static void displayContact() {
+		System.out.println("Enter the name of address book whose contacts you wanna display");
+
+		String bookName = r.nextLine();
+		AddressBook adBook = addressBook.get(bookName);
+
+		if (adBook != null) {
+			adBook.print();
+		} else {
+			System.out.println("Book name not found");
+		}
+
+	}
+
+	/*
+	 * method to add contact
+	 */
+	private static void addContact() {
+
+		System.out.println("Enter the name of Address book to which you wanna a add the contact");
+		// Scanner r =new Scanner(System.in);
+		String adBook = r.nextLine();
+		AddressBook Book = addressBook.get(adBook);
+		if (Book == null) {
+			System.out.println("No book found");
+
+		} else {
+			addressBook.get(adBook).addContact();
+		}
+
+	}
+
+}
